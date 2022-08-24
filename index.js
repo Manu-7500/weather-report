5bcb8cb8eb5a0f7fe9bb080bc2ea6e5a26ed28bb
MANU BHARADWAJ <pandatmanu318@gmail.com>
2022-08-24 14:55:16 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const search = document.querySelector( "#search" )
+// const weather = document.querySelector( "#weather" )
+// const submit = document.querySelector( "#submit" )
+
+// weather.addEventListener( "submit",
+//     function ( event ) {
+//         console.log( search.value )
+//         event.preventDefault();
+//     }
+// )
+
+
