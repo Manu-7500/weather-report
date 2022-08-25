b685aa7788c6dea5c43d1977f748a4b44efa2e55
MANU BHARADWAJ <pandatmanu318@gmail.com>
2022-08-25 13:28:20 +0300

    modify some js functionalities

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-// const search = document.querySelector( "#search" )
-// const weather = document.querySelector( "#weather" )
-// const submit = document.querySelector( "#submit" )
+const API_key = `5430d0b7021704517da30f91eb9b274f`
+const form = document.querySelector( "form" )
+const search = document.querySelector( "#search" )
+const weather = document.querySelector( "#weather" )
 
-// weather.addEventListener( "submit",
-//     function ( event ) {
-//         console.log( search.value )
-//         event.preventDefault();
-//     }
-// )
+const getweather = async ( city ) => {
+    weather.innerHTML = `<h2>Loading...</h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`
+    const response = await fetch( url );
+    const data = await response.json()
+    return showweather( data )
+}
 
+const showweather = ( data ) => {
+    if ( data.cod == "404" ) {
+        weather.innerHTML = `<h2> City Not Found <br> Please Enter a valid city Name <br> Thankyou for visit </h2>`
+    }
 
+    weather.innerHTML = `
+             <div>
+                <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" width="50px" height="50px" , alt="Not supported">
+            </div>
+                <div>
+                    <h2>${data.main.temp}°C </h2>
+                    <h4>${data.weather[0].main}</h4>
+                </div>`
+}
+
+form.addEventListener( "submit",
+    function ( event ) {
+        getweather( search.value )
+        event.preventDefault();
+    }
+)
